e0d4e013c50a5a4c669da539ed542a7472e57eef
Bhendon Soares <bhendon.soares12@gmail.com>
2024-07-25 15:41:57 -0300

    feat: adiciona tabela, entidade e repository de participante

---
+++ src/main/java/com/rocketseat/planner_api/participant/ParticipantRepository.java
@@ -0,0 +1,10 @@
+package com.rocketseat.planner_api.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+}
