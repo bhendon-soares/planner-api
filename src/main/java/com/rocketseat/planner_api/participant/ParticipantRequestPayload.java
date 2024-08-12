aec8eeed793b4236fa1e2cb51a921049c732a628
Bhendon Soares <bhendon.soares12@gmail.com>
2024-08-12 12:47:38 -0300

    feat: adiciona funcionalidade de confirmação de participante

---
+++ src/main/java/com/rocketseat/planner_api/participant/ParticipantRequestPayload.java
@@ -0,0 +1,4 @@
+package com.rocketseat.planner_api.participant;
+
+public record ParticipantRequestPayload(String name, String email) {
+}
--- src/main/java/com/rocketseat/planner_api/trip/TripController.java
+++ src/main/java/com/rocketseat/planner_api/trip/TripController.java
@@ -26,7 +26,7 @@ public class TripController {
 
         this.tripRepository.save(newTrip);
 
-        this.participantService.registerParticipantesToEvent(payload.emails_to_invite(), newTrip.getId());
+        this.participantService.registerParticipantesToEvent(payload.emails_to_invite(), newTrip);
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
     }
